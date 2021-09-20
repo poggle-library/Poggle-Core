6cdffca2f6fad47234e54e21035c3f1c23caa516
Nick Morgan <nick@interstellaragency.co.uk>
2021-09-20 12:39:57 +0100

    Basic component setup.

--- src/BlogPost/BlogPost.tsx
+++ src/BlogPost/BlogPost.tsx
@@ -1,32 +1,102 @@
 import React from 'react';
 
 export interface BlogProps {
-    image: string,
-    title: string,
-    description: string
-    button: string,
-    location: string
+  width?: number;
+  height?: number;
+  image: string;
+  title: string;
+  description: string;
+  desLength: number;
+  button?: string;
+  location?: string;
+  btnColor: string;
+  btnText: string;
+  btnPx?: number;
+  btnPy?: number;
+  btnLength?: number;
+  fnSize?: number;
+  cardRadius?: number;
+  cardShadow?: boolean;
+  cardColor?: string;
 }
 
-interface trimmerProps {
-    text: string,
-    charLength: number,
-}
+export const BlogPost: React.FC<BlogProps> = ({
+  cardRadius,
+  width,
+  height,
+  image,
+  title,
+  description,
+  desLength,
+  button,
+  location,
+  btnColor,
+  btnText,
+  btnPx,
+  btnPy,
+  fnSize,
+  btnLength,
+  cardShadow,
+  cardColor,
+}) => {
+  const textTrimmer = (text: string, charLength: number) => {
+    return text.slice(0, charLength).concat('...');
+  };
 
-const textTrimmer = ( text, charLength : trimmerProps) => {
-    if(text.length > charLength) {
-        return text.slice(0,charLength).concat("...")
-    }
-}
-
-export const BlogPost: React.FC<BlogProps> = ({image, title, description, button, location}) => {
-    return (
-        <div style={{backgroundColor: 'white', width: "30rem", height: "35rem", borderRadius: "20px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}>
-            <img src={image} alt="blog-post" style={{width: "100%", height:"15rem", objectFit: "cover", borderRadius: "20px 20px 0 0 "}} />
-            <h1 style={{padding: "0 1rem"}}>{title}</h1>
-            <p style={{padding: "0 1rem", fontSize: "18px"}}>{description.length > 400 ? textTrimmer(description, 400) : description}</p>
-            <button style={{padding: "0.5rem", backgroundColor: "purple", borderRadius: "10px", color: "white", marginLeft: "10px"}} onClick={() => console.log(location)}>{button}</button>
-
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        backgroundColor: cardColor ? cardColor : 'white',
+        width: `${width ? width : 25}rem`,
+        height: `${height ? height : 35}rem`,
+        borderRadius: `${cardRadius ? cardRadius + 'rem' : '1rem'}`,
+        boxShadow: cardShadow
+          ? '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+          : '',
+      }}
+    >
+      <img
+        src={image}
+        alt="blog-post"
+        style={{
+          width: '100%',
+          height: '15rem',
+          objectFit: 'cover',
+          borderRadius: `${
+            cardRadius
+              ? `${cardRadius}rem ${cardRadius}rem 0 0`
+              : '1rem 1rem 0 0 '
+          }`,
+        }}
+      />
+      <div
+        style={{
+          padding: '0 1rem',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <h1>{title}</h1>
+        <p style={{ fontSize: `${fnSize ? fnSize + 'rem' : '1rem'}` }}>
+          {description.length > desLength
+            ? textTrimmer(description, desLength)
+            : description}
+        </p>
+        {button && (
+          <button
+            style={{
+              padding: `${btnPy}rem ${btnPx}rem`,
+              backgroundColor: btnColor,
+              borderRadius: '10px',
+              color: btnText,
+              width: btnLength ? `${btnLength}rem` : '100%',
+            }}
+            onClick={() => console.log(location)}
+          >
+            {button}
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
--- stories/Thing.stories.tsx
+++ stories/Thing.stories.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { BlogPost } from '../src';
 
@@ -19,7 +19,27 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story = () => <BlogPost button="Read More" description={"Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam id dui posuere blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur aliquet quam id dui posuere blandit."} title={"This is the title of your blog post."} image={"https://images.pexels.com/photos/5679150/pexels-photo-5679150.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"}/>;
+const Template: Story = () => (
+  <BlogPost
+    cardShadow
+    btnLength={10}
+    cardRadius={1}
+    fnSize={1.2}
+    desLength={100}
+    btnPx={2}
+    btnPy={1}
+    btnText="white"
+    btnColor="black"
+    button="Read More"
+    description={
+      'Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam id dui posuere blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur aliquet quam id dui posuere blandit.'
+    }
+    title={'This is the title of your blog post.'}
+    image={
+      'https://images.pexels.com/photos/5679150/pexels-photo-5679150.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    }
+  />
+);
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
