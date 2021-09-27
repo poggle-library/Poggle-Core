59c1eb2e045ede9dd6b3f49764755d7aab518576
Nick Morgan <nick@interstellaragency.co.uk>
2021-09-27 19:57:35 +0100

    Material Design Colours Done!

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,5 @@ export * from './profile/profile.component';
 export * from './intro/intro.components';
 export * from './Contributors/contributors.component';
 export * from './containers/containers.container';
+// @ts-ignore
+export * from './colours/index.js';
